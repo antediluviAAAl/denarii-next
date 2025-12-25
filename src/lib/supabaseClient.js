f0b951151e00fca7e4be8a26730617224c199d79
antediluviAAAl <crisan.caius@gmail.com>
2025-12-25 03:49:17 +0200

    all

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://zdgswijfzycmriwbvhah.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpkZ3N3aWpmenljbXJpd2J2aGFoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM5MjY3ODgsImV4cCI6MjA3OTUwMjc4OH0.RP5MEmPvT4xCM-nt-WQAgVY72l-FWLUCTHqsA2hXhG0";
+const supabaseUrl = NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 export const supabase =
   supabaseUrl && supabaseKey ? createClient(supabaseUrl, supabaseKey) : null;
